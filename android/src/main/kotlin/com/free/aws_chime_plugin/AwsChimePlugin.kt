d892976d1a6fed62a36ab3843c588665cc630f3b
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2022-02-09 16:30:23 +0200

    WIP for android chime sdk

--- android/src/main/kotlin/com/free/aws_chime_plugin/AwsChimePlugin.kt
+++ android/src/main/kotlin/com/free/aws_chime_plugin/AwsChimePlugin.kt
@@ -1,35 +1,60 @@
 package com.free.aws_chime_plugin
 
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.util.Log
 import androidx.annotation.NonNull
-
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.EventChannel
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
 /** AwsChimePlugin */
-class AwsChimePlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+class AwsChimePlugin : FlutterPlugin, MethodCallHandler {
+    /// The MethodChannel that will the communication between Flutter and native Android
+    ///
+    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+    /// when the Flutter Engine is detached from the Activity
+    private var _methodChannel: MethodChannel? = null
+    private var _applicationContext: Context? = null
+    private var _eventSink: EventChannel.EventSink? = null
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "aws_chime_plugin")
-    channel.setMethodCallHandler(this)
-  }
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        val binaryMessenger = flutterPluginBinding.binaryMessenger
 
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+        _applicationContext = flutterPluginBinding.applicationContext
+
+        _methodChannel = MethodChannel(binaryMessenger, "aws_chime_plugin_method")
+        _methodChannel?.setMethodCallHandler(this)
+
+        val eventChannel = EventChannel(binaryMessenger, "aws_chime_plugin_events")
+        eventChannel.setStreamHandler(object : EventChannel.StreamHandler {
+            override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
+                _eventSink = events
+            }
+
+            override fun onCancel(arguments: Any?) {
+                Log.d(TAG, "EventChannel.setStreamHandler().onCancel()")
+            }
+        })
+
+        flutterPluginBinding.platformViewRegistry.registerViewFactory(
+            "AwsChimeRenderView",
+            AwsChimeRenderViewFactory()
+        )
     }
-  }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+        if (call.method == "getPlatformVersion") {
+            result.success("Android ${android.os.Build.VERSION.RELEASE}")
+        } else {
+            result.notImplemented()
+        }
+    }
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        _methodChannel?.setMethodCallHandler(null)
+    }
 }
